0a57c58b725a28a3b2fc57205b538c5a9b72c2d5
Stefan <dolhescu.stefan@gmail.com>
2020-10-31 20:47:53 +0200

    initial UI implememntation

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,5 +1,21 @@
 var web3 = new Web3(Web3.givenProvider);
+var contractInstance;
 
 $(document).ready(function() {
-    window.ethereum.enable();
+    window.ethereum.enable().then(function(accounts){
+        contractInstance = new web3.eth.Contract(abi, "0x699029e05BB93f8CAA3F4DbF859532586f6b3F3a", {from: accounts[0]});
+        console.log(contractInstance);
+    });
+    $("#bet_button").click(placeBet);
 });
+
+function placeBet() {
+  // alert("bet palced");
+  var betAmmount = $("#bet_input").val();
+
+  var config = {
+    value: web3.utils.toWei(betAmmount,"ether")
+  };
+
+  contractInstance.methods.flipCoin().send(config);
+}
