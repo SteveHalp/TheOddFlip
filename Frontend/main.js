fe5005bc6afc355c09d5e4ab48a5e89c13dd00e1
Stefan <dolhescu.stefan@gmail.com>
2020-11-02 21:51:43 +0200

    minor tweaks, this time with all staged changes :)

--- Frontend/main.js
+++ Frontend/main.js
@@ -3,15 +3,22 @@ var contractInstance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-        contractInstance = new web3.eth.Contract(abi, "0x699029e05BB93f8CAA3F4DbF859532586f6b3F3a", {from: accounts[0]});
+        contractInstance = new web3.eth.Contract(abi, "0x55795dBB92D0E3Dff089CE6EC8A3fbb5E28a0331", {from: accounts[0]});
         console.log(contractInstance);
     });
     $("#bet_button").click(placeBet);
 });
 
 function placeBet() {
-  // alert("bet palced");
   var betAmmount = $("#bet_input").val();
+
+  //This sort of validation I would usually do in the backend. As some cheeky user might just remove this bit of code using the browser inspector.
+  //In the same time, I saw some solutions in Solidity for converting strings to int or checking if strings can be converted to int. But to me personally it would seem as a waste of gas to perfrom such operations in a smart contracts.
+  //Ideally I would have a middle layer in the backed that would actually be called by the front-end, which would execute all necessary validation on the input data and only afterwards, if data is valid, I would call the smart contract.
+  if (isNaN(betAmmount)){
+         alert("Please provide the input as a number");
+         return false;
+      }
 
   var config = {
     value: web3.utils.toWei(betAmmount,"ether")
