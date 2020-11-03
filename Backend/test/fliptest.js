04d44c9bdcbdca6c03f0a9f6f5281ab8af6f26f1
Stefan <dolhescu.stefan@gmail.com>
2020-11-04 00:31:47 +0200

    add bet restrictions
    add more validation on UI
    adjust unit tests

--- Backend/test/fliptest.js
+++ Backend/test/fliptest.js
@@ -7,32 +7,30 @@ contract("Flip", async function(accounts){
 
   before(async function() {
     instance = await Flip.deployed();
-    // await instance.sendTransaction({ from: accounts[2], value: web3.utils.toWei("5","ether")})
-    // await instance.send(web3.utils.toWei("3","ether"), {from: accounts[0]});
   });
 
   it("should return no ammount or double the ammount sent", async function(){
 
-    let initialBalance = parseInt(web3.utils.fromWei(await instance.balance(),"milliether"));
-    let betAmmount = web3.utils.toWei("10","milliether");
+    var initialBalance = web3.utils.fromWei(await instance.balance(),"milliether");
+    var betAmmount = web3.utils.toWei("10","milliether");
 
-    await instance.flipCoin({value: betAmmount, from: accounts[1]});
+    await instance.flipCoin(1, {value: betAmmount, from: accounts[1]});
 
-    let finalBalance = parseInt(web3.utils.fromWei(await instance.balance(),"milliether"));
+    var finalBalance = web3.utils.fromWei(await instance.balance(),"milliether");
 
-    // let initialBalance = await instance.balance();
-    // let betAmmount = web3.utils.toWei("10","milliether");
-    //
-    // await instance.flipCoin({value: betAmmount, from: accounts[1]});
-    //
-    // let finalBalance =  await instance.balance();
+    console.log("bet ammount: ", web3.utils.fromWei(betAmmount,"milliether")," milliether");
 
-    console.log("bet ammount: ", betAmmount);
+    console.log("initial balance: ", initialBalance, " milliether");
+    console.log("final balance: ", finalBalance, " milliether");
 
-    console.log("initial balance: ", initialBalance);
-    console.log("final balance: ", finalBalance);
+    var difference = Math.abs(initialBalance - finalBalance);
 
-    assert(finalBalance === (initialBalance - betAmmount) || finalBalance === (initialBalance + betAmmount), "contract balance didn't change");
+    console.log("difference: ", difference, " milliether");
+
+    assert(
+      difference == new Number(web3.utils.fromWei(betAmmount,"milliether")),
+      "contract balance didn't change according to a win or loss"
+    );
   });
 
   it("withdrawal from contract balance leaves no funds in the contract, while the owner contract blance increases accordingly", async function(){
@@ -50,6 +48,8 @@ contract("Flip", async function(accounts){
 
     console.log("final contract balance:", finalContractBalance);
     console.log("final account balance:", finalAccountBalance);
+
+    assert(finalContractBalance == 0, "contract balance is not zero");
   });
 
   it("should allow withdrawal from contract balance only by contract owner", async function(){
@@ -58,13 +58,5 @@ contract("Flip", async function(accounts){
 
   it("should fail withdrawal from contract balance by non-owner of contract", async function(){
     await truffleAssert.fails(instance.withdrawAll({from: accounts[1]}), truffleAssert.ErrorType.REVERT);
-  });
-
-  it("should return caller details", async function(){
-
-      let result = await instance.getUserDetails();
-
-      console.log("user balance:", parseInt(result.balance));
-      console.log("user address:", result.userAddress);
   });
 });
